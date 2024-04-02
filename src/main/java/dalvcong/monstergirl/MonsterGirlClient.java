9dad20c5673fb65124be822f0da5ff2ca953a786
da_lv_cong_ <2461376697@qq.com>
2024-04-02 13:12:00 +0800

    Initial commit

---
+++ src/main/java/dalvcong/monstergirl/MonsterGirlClient.java
@@ -0,0 +1,10 @@
+package dalvcong.monstergirl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MonsterGirlClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
